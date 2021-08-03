2b686023aaca4600e2e2602346d06b641615dce3
Md. Rifat Islam <xxxxxxrifatxxxxxx@gmail.com>
2021-08-04 03:19:27 +0600

    Add Factorial Comment

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+// Factorial
+// 1! = 1
+// 2! = 2 X 1
+// 3! = 3 X 2 X 1
+// 4! = 4 X 3 X 2 X 1
+// 5! = 5 X 4 X 3 X 2 X 1
+
+// Recursive Factorial
+// 1! = 1
+// 2! = 2 X 1!
+// 3! = 3 X 2!
+// 4! = 4 X 3!
+// 5! = 5 X 4!
+
 // Define UI
 var number = document.querySelector("#number");
 var result = document.querySelector("#result");
